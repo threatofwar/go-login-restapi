c01a1e61ee899e0e80e66d3e1cea8abd5032ebee
Adlil Yaacob <adlil.yaacob@gmail.com>
2025-01-04 13:31:04 +0000

    add cors config

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"go-login-restapi/auth"
 	"go-login-restapi/handlers"
 	"go-login-restapi/pkg/db"
 	"go-login-restapi/pkg/db/models"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 	// load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	// ALLOWORIGINS_URL := os.Getenv("ALLOWORIGINS_URL")
-	// allowedOrigins := strings.Split(ALLOWORIGINS_URL, ",")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	ALLOWORIGINS_URL := os.Getenv("ALLOWORIGINS_URL")
+	allowedOrigins := strings.Split(ALLOWORIGINS_URL, ",")
 
 	// Initialize database
 	db.InitDB()
@@ -32,14 +32,14 @@ func main() {
 	router := gin.Default()
 
 	// CORS config
-	// router.Use(cors.New(cors.Config{
-	// 	AllowOrigins:     allowedOrigins,
-	// 	AllowMethods:     []string{"GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"},
-	// 	AllowHeaders:     []string{"Content-Type", "Authorization"},
-	// 	ExposeHeaders:    []string{},
-	// 	AllowCredentials: true,
-	// 	MaxAge:           12 * 3600,
-	// }))
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     allowedOrigins,
+		AllowMethods:     []string{"GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{},
+		AllowCredentials: true,
+		MaxAge:           12 * 3600,
+	}))
 
 	// routes without auth
 	router.GET("/hello", func(c *gin.Context) {
